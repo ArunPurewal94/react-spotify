cf33f72830e79687e9c5c0d45ca748ab1adb87e5
Ashleigh Carr <Ashleigh.Carr@twohouses.co.uk>
2020-05-26 15:33:52 +0100

    Updates

---
+++ precache-manifest.6d398f3e2997fb3727c7b78e7c9483c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "169619a155c916551c9b",
+    "url": "/react-spotify/static/css/main.c2e16573.chunk.css"
+  },
+  {
+    "revision": "169619a155c916551c9b",
+    "url": "/react-spotify/static/js/main.7dad058e.chunk.js"
+  },
+  {
+    "revision": "4cfa1170b08206d4c844",
+    "url": "/react-spotify/static/js/runtime~main.810ade09.js"
+  },
+  {
+    "revision": "eac729734845c2f37e11",
+    "url": "/react-spotify/static/js/2.faa277a1.chunk.js"
+  },
+  {
+    "revision": "9f084358738e12aed18dfc9b6db18042",
+    "url": "/react-spotify/static/media/playlist.9f084358.png"
+  },
+  {
+    "revision": "ea524865863a06553421e4610874b06a",
+    "url": "/react-spotify/static/media/media.ea524865.png"
+  },
+  {
+    "revision": "a6ca1d453dc8b2bbb57a6254f57b839c",
+    "url": "/react-spotify/static/media/profile.a6ca1d45.png"
+  },
+  {
+    "revision": "30418606616301eaf2d3af55d2801596",
+    "url": "/react-spotify/index.html"
+  }
+];
